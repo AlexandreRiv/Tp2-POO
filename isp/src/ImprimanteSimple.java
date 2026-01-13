7080f61dbf10e518d175d4369309747c417f9040
Alexandre <alexandreriviere111@gmail.com>
2026-01-13 12:00:58 +0100

    ISP exo

--- isp/src/ImprimanteSimple.java
+++ isp/src/ImprimanteSimple.java
@@ -1,14 +1,5 @@
-public class ImprimanteSimple implements Machine {
-
+public class ImprimanteSimple implements Printer {
     public void print() {
         System.out.println("Impression en cours...");
-    }
-
-    public void scan() {
-        throw new UnsupportedOperationException();
-    }
-
-    public void fax() {
-        throw new UnsupportedOperationException();
     }
 }
---
+++ isp/src/Scanne.java
@@ -0,0 +1,3 @@
+public interface Scanne {
+    void scan();
+}
